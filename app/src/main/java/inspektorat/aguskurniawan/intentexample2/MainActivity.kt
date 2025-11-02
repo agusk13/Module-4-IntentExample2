708e6a4af0d9f9f635cdedb4fa7ae0ec85911f49
agusk13 <agusk1308@gmail.com>
2025-11-02 13:16:29 +0700

    implementasi mengirim data melalui Intent

--- app/src/main/java/inspektorat/aguskurniawan/intentexample2/MainActivity.kt
+++ app/src/main/java/inspektorat/aguskurniawan/intentexample2/MainActivity.kt
@@ -1,20 +1,30 @@
 package inspektorat.aguskurniawan.intentexample2
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import inspektorat.aguskurniawan.intentexample2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            val sendData = binding.editText.text.toString()
+            val intent = Intent(
+                this@MainActivity,
+                SecondActivity::class.java
+            )
+            intent.putExtra("main_activity_data", sendData)
+            startActivity(intent)
         }
     }
 }
